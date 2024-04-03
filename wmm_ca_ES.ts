41b7a8e6d6b4ad6adcde28682f497bf2d9603a7a
Xavier Carol <xcarolr@gmail.com>
2024-04-03 12:49:58 +0200

    will work on langs later

--- wmm_ca_ES.ts
+++ wmm_ca_ES.ts
@@ -1,15 +1,3 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ca_ES">
-<context>
-  <name>QFileDialog</name>
-  <message>
-    <source>Open File</source>
-    <translation>Obre Arxiu</translation>
-  </message>
-  <message>
-    <source>Csv Files (*.csv)</source>
-    <translation>Arxius Csv (*.csv)</translation>
-  </message>
-</context>
-</TS>
+<TS version="2.1" language="ca_ES"></TS>
