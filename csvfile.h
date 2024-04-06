4936e6a031c66727d798018be24940906986ef10
Xavier Carol <xcarolr@gmail.com>
2024-04-06 15:03:24 +0200

    add isEmpty helper method

--- csvfile.h
+++ csvfile.h
@@ -14,6 +14,7 @@ public:
     static bool isValidCsvFile(QString filename);
 
 public:
+    bool isEmpty() { return csvRows.isEmpty(); }
     bool read(QString filename);
     QList<QStringList> getRows(int);
 };
