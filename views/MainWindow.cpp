aebd2a951fb09643f294a2df6505c101fed2290a
Xavier Carol <xcarolr@gmail.com>
2024-04-08 12:54:02 +0200

    add menu option to switch between views and save last active view

--- views/MainWindow.cpp
+++ views/MainWindow.cpp
@@ -1,13 +1,21 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
 #include "../dialogs/DatabaseSettings.h"
-#include "ImportFileView.h"
 #include "../helpers/Database.h"
+#include "CategorizeView.h"
+#include "ImportFileView.h"
+#include <QSettings>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
-  setCentralWidget(new ImportFileView());
+
+  QSettings settings = QSettings("com.xicra", "wmm");
+  if (settings.value(CURRENT_VIEW).toString() == CATEGORIZE_VIEW) {
+    setCentralWidget(new CategorizeView());
+  } else {
+    setCentralWidget(new ImportFileView());
+  }
 }
 
 MainWindow::~MainWindow() { delete ui; }
@@ -16,10 +24,8 @@ void MainWindow::on_actionDatabase_triggered() {
   DatabaseSettings dialog = DatabaseSettings();
   Database database = Database();
 
-  dialog.init(database.getHostname(),
-              database.getDatabase(),
-              database.getUsername(),
-              database.getUserpass());
+  dialog.init(database.getHostname(), database.getDatabase(),
+              database.getUsername(), database.getUserpass());
 
   bool success = dialog.exec();
   if (success) {
@@ -28,4 +34,16 @@ void MainWindow::on_actionDatabase_triggered() {
     database.setUsername(dialog.username);
     database.setUserpass(dialog.userpass);
   }
+}
+
+void MainWindow::on_actionImport_triggered() {
+  QSettings settings = QSettings("com.xicra", "wmm");
+  settings.setValue(CURRENT_VIEW, IMPORT_VIEW);
+  setCentralWidget(new ImportFileView());
+}
+
+void MainWindow::on_actionCategorize_triggered() {
+  QSettings settings = QSettings("com.xicra", "wmm");
+  settings.setValue(CURRENT_VIEW, CATEGORIZE_VIEW);
+  setCentralWidget(new CategorizeView());
 }
