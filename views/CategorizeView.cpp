40558d6b8f8929e3bddb7bf07917e2846e9a5724
Xavier Carol <xcarolr@gmail.com>
2024-04-08 20:21:42 +0200

    update rows

--- views/CategorizeView.cpp
+++ views/CategorizeView.cpp
@@ -8,6 +8,7 @@
 CategorizeView::CategorizeView(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::CategorizeView) {
   ui->setupUi(this);
+  ui->updateButton->setEnabled(false);
   ui->searchResultsTable->horizontalHeader()->setStretchLastSection(true);
 }
 
@@ -16,10 +17,14 @@ CategorizeView::~CategorizeView() { delete ui; }
 void CategorizeView::on_searchButton_clicked() {
   Database database = Database();
 
-  QStringList labels = database.getColumnNames();
-  QList<QStringList> uncategorizedRow = database.getUncategorizedRows(ui->filterEdit->text());
+  ui->searchResultsTable->clear();
+  ui->searchResultsTable->setRowCount(0);
+  ui->searchResultsTable->setColumnCount(0);
 
-  if (labels.length() == 0 || uncategorizedRow.length() == 0) {
+  QStringList labels = database.getColumnNames();
+  uncategorizedRows = database.getUncategorizedRows(appliedFilter);
+
+  if (labels.length() == 0 || uncategorizedRows.length() == 0) {
     QString error = database.getLastErrorText();
     if (!error.isEmpty()) {
       QMessageBox(QMessageBox::Icon::Critical, QString("Database error"),
@@ -29,10 +34,9 @@ void CategorizeView::on_searchButton_clicked() {
     return;
   }
 
-  int rowsCount = uncategorizedRow.count();
-  int columnsCount = uncategorizedRow.at(0).count();
+  int rowsCount = uncategorizedRows.count();
+  int columnsCount = uncategorizedRows.at(0).count();
 
-  ui->searchResultsTable->clear();
   ui->searchResultsTable->setRowCount(rowsCount);
   ui->searchResultsTable->setColumnCount(columnsCount);
   ui->searchResultsTable->verticalHeader()->setVisible(false);
@@ -45,7 +49,55 @@ void CategorizeView::on_searchButton_clicked() {
   for (int column = 0; column < columnsCount; column++) {
     for (int row = 0; row < rowsCount; row++) {
       ui->searchResultsTable->setCellWidget(
-          row, column, new QLabel(uncategorizedRow.at(row).at(column)));
+          row, column, new QLabel(uncategorizedRows.at(row).at(column)));
     }
   }
+
+  updateUpdateButtonState();
 }
+
+void CategorizeView::on_updateButton_clicked() {
+  QMessageBox::StandardButton res =
+      QMessageBox::question(this, QString("Update"),
+                            QString("You're about to update %1 records "
+                                    "with the category: %2\nAre you sure?")
+                                .arg(uncategorizedRows.length())
+                                .arg(categoryName));
+  if (res == QMessageBox::StandardButton::No) {
+    return;
+  }
+
+  Database database = Database();
+
+  QProgressDialog progress =
+      QProgressDialog("Update progress", "Cancel", 0, uncategorizedRows.length());
+
+  ulong updatedRows = database.updateRowsCategory(appliedFilter, categoryName);
+
+  progress.cancel();
+
+  if (updatedRows != uncategorizedRows.length()) {
+    QMessageBox(QMessageBox::Icon::Critical, QString("Database error"),
+                QString(database.getLastErrorText()))
+        .exec();
+  } else {
+    QMessageBox(QMessageBox::Icon::Information, QString("Database success"),
+                QString("A total of %1 rows updated").arg(updatedRows))
+        .exec();
+  }
+}
+
+void CategorizeView::on_categoryComboBox_editTextChanged(const QString &arg1) {
+  categoryName = arg1;
+  updateUpdateButtonState();
+}
+
+void CategorizeView::updateUpdateButtonState() {
+  ui->updateButton->setEnabled(uncategorizedRows.length() > 0 && categoryName.length() > 0);
+}
+
+void CategorizeView::on_filterEdit_textChanged(const QString &arg1)
+{
+  appliedFilter = arg1;
+}
+
--- views/CategorizeView.h
+++ views/CategorizeView.h
@@ -7,19 +7,30 @@ namespace Ui {
 class CategorizeView;
 }
 
-class CategorizeView : public QMainWindow
-{
-    Q_OBJECT
+class CategorizeView : public QMainWindow {
+  Q_OBJECT
 
 public:
-    explicit CategorizeView(QWidget *parent = nullptr);
-    ~CategorizeView();
+  explicit CategorizeView(QWidget *parent = nullptr);
+  ~CategorizeView();
 
 private slots:
-    void on_searchButton_clicked();
+  void on_searchButton_clicked();
 
-private:
-    Ui::CategorizeView *ui;
+  void on_updateButton_clicked();
+
+  void on_categoryComboBox_editTextChanged(const QString &arg1);
+
+  void on_filterEdit_textChanged(const QString &arg1);
+
+  private:
+  Ui::CategorizeView *ui;
+
+  QList<QStringList> uncategorizedRows;
+  QString categoryName;
+  QString appliedFilter;
+
+  void updateUpdateButtonState();
 };
 
 #endif // CATEGORIZEVIEW_H
