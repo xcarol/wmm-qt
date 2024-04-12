ea49499fb987b06c021df9a314d7364e1e6e7450
xcarol <xcarolr@gmail.com>
2024-04-12 18:51:38 +0200

    recreate the database if needed

--- database/database.sql
+++ database/database.sql
@@ -15,6 +15,10 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+DROP DATABASE IF EXISTS `wmm`;
+CREATE DATABASE `wmm`;
+USE `wmm`;
+
 --
 -- Table structure for table `transactions`
 --
