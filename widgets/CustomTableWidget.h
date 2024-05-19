5e924069ab4cd61621d7f4452f91a873087cb53d
xcarol <xcarolr@gmail.com>
2024-05-19 17:02:32 +0200

    use a new table based on our cutom table in browse data view

---
+++ widgets/CustomTableWidget.h
@@ -0,0 +1,42 @@
+#ifndef CUSTOMTABLEWIDGET_H
+#define CUSTOMTABLEWIDGET_H
+
+#include <QTableWidget>
+#include <QSettings>
+
+class CustomTableWidget : public QTableWidget {
+  Q_OBJECT
+
+private:
+  QString sortSymbolsRegexp = " (▲|▼)$";
+  QString sortSymbolDown = " ▼";
+  QString sortSymbolUp = " ▲";
+
+  bool resizeNastyReentering = false;
+  int sortedColumn = -1;
+  Qt::SortOrder sortOrder = Qt::SortOrder::AscendingOrder;
+
+  QString settingTemplate = QString("%1_column_%2");
+  QSettings settings = QSettings("com.xicra", "wmm");
+
+  void saveColumnsWidths();
+  void sortByColumn(int column);
+  void sortHeaders(int column, Qt::SortOrder order);
+
+protected:
+  QString tableName = QString("");
+
+  bool isNesting();
+  void disableNesting();
+  void enableNesting();
+  void restoreColumnsWidths(QString tableName);
+
+public:
+  explicit CustomTableWidget(QWidget *parent = nullptr);
+  void on_headerClicked(int index);
+  void on_headerResized();
+
+signals:
+};
+
+#endif // CUSTOMTABLEWIDGET_H
